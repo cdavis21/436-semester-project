b2bdc45d8740cf9ed29c781b4cc4816281f402a2
wonder-bread <ameasday@terpmail.umd.edu>
2014-12-03 15:24:58 -0500

    Added interaction with File I/O to save/load list items.

---
+++ 436-semester-project-master/src/com/worldstars/semesterproject436/Purchase.java
@@ -0,0 +1,54 @@
+package com.worldstars.semesterproject436;
+
+public class Purchase {
+	private String name;
+	private String cost;
+	private String category;
+	private String subcategory;
+	
+	public Purchase() {
+		name = "n/a";
+		cost = "n/a";
+		category = "n/a";
+		subcategory = "n/a";
+	}
+	
+	public Purchase(String name, String cost, String category, String subcategory) {
+		this.name = name;
+		this.cost = cost;
+		this.category = category;
+		this.subcategory = subcategory;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setCost(String cost) {
+		this.cost = cost;
+	}
+	
+	public String getCost() {
+		return this.cost;
+	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public String getCategory() {
+		return this.category;
+	}
+	
+	public void setSubcategory(String subcategory) {
+		this.subcategory = subcategory;
+	}
+	
+	public String getSubcategory() {
+		return this.subcategory;
+	}
+}
