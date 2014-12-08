ff209824c16b505f76dad0a27a13e5c5b3c12dbd
cdavis21 <trainedballa@Courtney-Daviss-MacBook-Pro.local>
2014-12-08 02:57:07 -0500

    fixed add tab

---
+++ src/com/worldstars/semesterproject436/DetailFragment.java
@@ -0,0 +1,78 @@
+package com.worldstars.semesterproject436;
+
+import java.text.NumberFormat;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailFragment extends Fragment{
+	private String name;
+	private String cost;
+	private String category;
+	private String subcategory;
+	private int icon;
+
+	public DetailFragment(){
+		
+	}
+	
+	 @Override
+	 public void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+	   
+	 }
+	  
+	 @Override
+	 public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			NumberFormat nf = NumberFormat.getCurrencyInstance();
+			
+			View view = inflater.inflate(R.layout.activity_detail, container, false);
+			System.out.println("tester");
+			System.out.println(getArguments().getString("Name"));
+			
+			Purchase p = new Purchase(getArguments().getString("Name"),
+					getArguments().getString("Cost"), getArguments().getString("Cat"), 
+					getArguments().getString("Subcat") , getArguments().getInt("Icon"));
+			
+			TextView tvName = (TextView) view.findViewById(R.id.NameView);
+			tvName.setText(p.getName());
+
+			TextView tvCost = (TextView) view.findViewById(R.id.CostView);
+			tvCost.setText(p.getCost());
+			
+			TextView tvcategory = (TextView) view.findViewById(R.id.CategoryView);
+			tvcategory.setText(p.getCategory());
+			
+			TextView tvsubcategory = (TextView) view.findViewById(R.id.SubcategoryView);
+			tvsubcategory.setText(p.getSubcategory());
+			
+			
+			
+			TextView tvChina = (TextView) view.findViewById(R.id.ChinaView);
+			tvChina.setText(nf.format((p.calculateChina(p.getCost()))));
+
+			TextView tvBrazil = (TextView) view.findViewById(R.id.BrazilView);
+			tvBrazil.setText(nf.format((p.calculateBrazil(p.getCost()))));
+
+			
+			TextView tvIndia = (TextView) view.findViewById(R.id.IndiaView);
+			tvIndia.setText(nf.format((p.calculateIndia(p.getCost()))));
+
+			
+			TextView tvIndonesia = (TextView) view.findViewById(R.id.IndonesiaView);
+			tvIndonesia.setText(nf.format((p.calculateIndonesia(p.getCost()))));
+
+			
+			TextView tvPakistan = (TextView) view.findViewById(R.id.PakistanView);
+			tvPakistan.setText(nf.format((p.calculatePakistan(p.getCost()))));
+		 return view;
+	 }
+		
+}
